3045f67d77eeb11855b69c21cd172c710dad5d9d
Ziaullah Khan <ziaullahkhan.se@gmail.com>
2026-08-11 21:30:54 +0500

    done

---
+++ ComparisonOperatiorsGradeChecker.py
@@ -0,0 +1,9 @@
+a=int(input("Enter your marks(0-100): "))
+if a>=80:
+    print("Grade A!")
+elif a>=60 and a<80:
+    print("Grade B!")
+elif a>=40 and a<60:
+    print("Grade C!")
+elif a<40:
+    print("Fail")
