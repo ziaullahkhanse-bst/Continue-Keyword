0bebc35027726e34362f64b7a409692610cc940b
Ziaullah Khan <ziaullahkhan.se@gmail.com>
2026-08-12 22:57:42 +0500

    Delete ComparisonOperatiorsGradeChecker.py

--- ComparisonOperatiorsGradeChecker.py
+++
@@ -1,9 +0,0 @@
-a=int(input("Enter your marks(0-100): "))
-if a>=80:
-    print("Grade A!")
-elif a>=60 and a<80:
-    print("Grade B!")
-elif a>=40 and a<60:
-    print("Grade C!")
-elif a<40:
-    print("Fail")
